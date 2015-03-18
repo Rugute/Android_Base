f8991d74c056fa9ae10a0ec2cff5cd0d6b53d8d3
Dale Zak <dalezak@gmail.com>
2015-03-17 23:21:36 -0600

    0.77

--- library/src/main/java/ca/dalezak/androidbase/tasks/CardTask.java
+++ library/src/main/java/ca/dalezak/androidbase/tasks/CardTask.java
@@ -5,10 +5,13 @@ import android.content.Intent;
 import android.os.AsyncTask;
 
 import ca.dalezak.androidbase.models.BaseModel;
+import ca.dalezak.androidbase.utils.Log;
+import ca.dalezak.androidbase.utils.ProgressDialog;
 import ca.dalezak.androidbase.views.BaseCard;
 
 public abstract class CardTask<M extends BaseModel, C extends BaseCard<M>> extends AsyncTask<Void, Void, Intent> {
 
+    private ProgressDialog dialog;
     private Context context;
     private M model;
     private C card;
@@ -19,19 +22,41 @@ public abstract class CardTask<M extends BaseModel, C extends BaseCard<M>> exten
         this.model = model;
     }
 
+    public CardTask(Context context, C card, M model, int message) {
+        this.context = context;
+        this.card = card;
+        this.model = model;
+        try {
+            dialog = new ProgressDialog(context);
+            dialog.setMessage(context.getString(message));
+            dialog.setIndeterminate(true);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        }
+        catch (Exception exception) {
+            Log.w(this, "Exception", exception);
+        }
+    }
+
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
         card.setSelected(true);
+        if (dialog != null) {
+            dialog.show();
+        }
     }
 
     @Override
     protected void onPostExecute(Intent intent) {
         super.onPostExecute(intent);
+        card.setSelected(false);
+        if (dialog != null) {
+            dialog.hide();
+            dialog = null;
+        }
         if (intent != null) {
             context.startActivity(intent);
         }
-        card.setSelected(false);
     }
 
     protected Context getContext() {
@@ -44,5 +69,13 @@ public abstract class CardTask<M extends BaseModel, C extends BaseCard<M>> exten
 
     protected C getCard() {
         return card;
+    }
+
+    protected boolean hasDialog() {
+        return dialog != null;
+    }
+
+    protected ProgressDialog getDialog() {
+        return dialog;
     }
 }
