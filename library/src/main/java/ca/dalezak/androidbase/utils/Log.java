0b1c73383f26f0e09bf34522d22cc9fbb46e977f
Dale Zak <dalezak@gmail.com>
2015-04-22 15:10:39 -0600

    1.38

--- library/src/main/java/ca/dalezak/androidbase/utils/Log.java
+++ library/src/main/java/ca/dalezak/androidbase/utils/Log.java
@@ -5,7 +5,10 @@ public class Log {
     private static Callback callback;
 
     public interface Callback {
-        public void onCaughtException(Throwable throwable);
+        public void onLogInfo(String tag, String message);
+        public void onLogDebug(String tag, String message);
+        public void onLogWarning(String tag, String message);
+        public void onLogException(Throwable throwable);
     }
 
     public static Callback getCallback() {
@@ -18,47 +21,73 @@ public class Log {
 
     public static void i(Object caller, String message) {
         android.util.Log.i(getName(caller), message);
+        if (callback != null) {
+            callback.onLogInfo(getName(caller), message);
+        }
     }
 
     public static void i(Object caller, String message, Throwable throwable) {
         android.util.Log.i(getName(caller), message, throwable);
+        if (callback != null) {
+            callback.onLogInfo(getName(caller), message);
+        }
     }
 
     public static void i(Object caller, String format, Object...args) {
         android.util.Log.i(getName(caller), String.format(format, args));
+        if (callback != null) {
+            callback.onLogInfo(getName(caller), String.format(format, args));
+        }
     }
 
     public static void d(Object caller, String message) {
         android.util.Log.d(getName(caller), message);
+        if (callback != null) {
+            callback.onLogDebug(getName(caller), message);
+        }
     }
 
     public static void d(Object caller, String message, Throwable throwable) {
         android.util.Log.d(getName(caller), message, throwable);
+        if (callback != null) {
+            callback.onLogDebug(getName(caller), message);
+        }
     }
 
     public static void d(Object caller, String format, Object...args) {
         android.util.Log.d(getName(caller), String.format(format, args));
+        if (callback != null) {
+            callback.onLogDebug(getName(caller), String.format(format, args));
+        }
     }
 
     public static void w(Object caller, String message) {
         android.util.Log.w(getName(caller), message);
+        if (callback != null) {
+            callback.onLogWarning(getName(caller), message);
+        }
     }
 
     public static void w(Object caller, String message, Throwable throwable) {
         android.util.Log.w(getName(caller), message, throwable);
+        if (callback != null) {
+            callback.onLogWarning(getName(caller), message);
+        }
     }
 
     public static void w(Object caller, String format, Object...args) {
         android.util.Log.w(getName(caller), String.format(format, args));
+        if (callback != null) {
+            callback.onLogWarning(getName(caller), String.format(format, args));
+        }
     }
-
 
     public static void e(Object caller, String message) {
         android.util.Log.e(getName(caller), message);
         if (callback != null) {
             String description = String.format("%s %s", getName(caller), message);
             Exception exception = new Exception(description);
-            callback.onCaughtException(exception);
+            callback.onLogException(exception);
         }
     }
 
@@ -67,7 +96,7 @@ public class Log {
         if (callback != null) {
             String description = String.format("%s %s", getName(caller), message);
             Exception exception = new Exception(description, throwable);
-            callback.onCaughtException(exception);
+            callback.onLogException(exception);
         }
     }
 
@@ -76,7 +105,7 @@ public class Log {
         if (callback != null) {
             String description = String.format("%s %s", getName(caller), String.format(format, args));
             Exception exception = new Exception(description);
-            callback.onCaughtException(exception);
+            callback.onLogException(exception);
         }
     }
 
