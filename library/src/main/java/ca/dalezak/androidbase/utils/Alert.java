19538aa0d2e5e3ad6b6ed19b6506c8620ec9536a
Dale Zak <dalezak@gmail.com>
2015-09-23 17:17:31 -0600

    1.57

--- library/src/main/java/ca/dalezak/androidbase/utils/Alert.java
+++ library/src/main/java/ca/dalezak/androidbase/utils/Alert.java
@@ -1,8 +1,8 @@
 package ca.dalezak.androidbase.utils;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 
 import ca.dalezak.androidbase.R;
 
